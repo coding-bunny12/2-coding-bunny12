1bb6e2f88343f38a4e64e7cc2281609b8806febb
coding-bunny12 <ahnyei@students.unwsp.edu>
2026-02-01 20:54:39 -0600

    Add files via upload

---
+++ program1 w2.py
@@ -0,0 +1,19 @@
+# Abrielle Nyei
+# 1/30/26
+# Personal information
+def personal_information():
+    name = 'Alex Anderson'
+    street = '123 Galaxy Lane'
+    city = 'Dreamtown'
+    state = 'MN'
+    zip_code = '55123'
+    phone = '(555) 123-4567'
+    major = 'Art Design'
+# line that separates function
+    print("Name:", name)
+    print("Address:", street)
+    print("City/State/Zip:", city + "," + state + "," + zip_code)
+    print("Phone:", phone)
+    print("College Major:", major)
+# Line which calls the above function.
+personal_information()
